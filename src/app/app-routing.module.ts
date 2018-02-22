ef4c36e1f9f8b69fa70ee2aa823a33ef4bc14a52
Patrick Cagnoni <edmacdev@gmail.com>
2018-02-22 13:18:15 -0300

    course page route added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-
 import { LoginComponent } from './components/login/login.component';
 import { HomeComponent } from './components/home/home.component';
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { ProfileComponent } from './components/profile/profile.component';
-import { UserComponent } from './components/user/user.component'
+import { UserComponent } from './components/user/user.component';
+import { CoursePageComponent } from './components/course-page/course-page.component';
 
 import { AuthGuard } from './guards/auth.guard';
 
@@ -23,8 +23,8 @@ const routes: Routes = [
     canActivate: [AuthGuard]
   },
   {
-    path: 'user/:id/:moodle',
-    component: UserComponent
+    path: 'curso/:id',
+    component: CoursePageComponent
   },
   {
     path: '',
